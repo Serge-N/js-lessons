4c7c401fa11219fdfeaf57d5e63b082e726a05e3
Serge-N <nserge05@mail.com>
2021-06-22 11:04:17 +0200

    Create T&O-Classes.js

---
+++ ydkjs/T&O-Classes.js
@@ -0,0 +1,18 @@
+// constructor
+
+// exmple of inheritance
+
+//  base class
+class Vechicle {
+    engines = 1;
+
+    iginition(){
+        console.log("Turning on my engine.")
+    }
+
+    drive(){
+        this.iginition();
+        console.log("Sterering and moving forward!");
+    }
+}
+
