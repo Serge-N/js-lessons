ac11692ab9c473943cbe7ea711ba93381fb262d2
Serge-N <nserge05@gmail.com>
2021-07-05 23:01:56 +0200

    prototytpal In heritance

--- ydkjs/T&O-Prototypes.js
+++ ydkjs/T&O-Prototypes.js
@@ -2,12 +2,12 @@
 // a prototyped object is an object linked to another object
 
 var anotherObject = {
-    a : 2
+    a: 2
 };
 
-var myObject = Object.create( anotherObject );
+var myObject = Object.create(anotherObject);
 
-console.log( myObject.a );
+console.log(myObject.a);
 
 // if the prototype chain ends or not found - undefined is returned
 
@@ -15,19 +15,19 @@ console.log( myObject.a );
 
 // avoid shadowing if possible - the consequences are greater
 
-console.log( anotherObject.hasOwnProperty( "a" ) ); 
-console.log( myObject.hasOwnProperty( "a" ) ); 
+console.log(anotherObject.hasOwnProperty("a"));
+console.log(myObject.hasOwnProperty("a"));
 
 myObject.a++; // oops, implicit shadowing!
 
-console.log( anotherObject.a ); 
-console.log( myObject.a ); 
+console.log(anotherObject.a);
+console.log(myObject.a);
 
-console.log( myObject.hasOwnProperty( "a" ) );
+console.log(myObject.hasOwnProperty("a"));
 
 // keyword new hijacks any normal function
 
-function NothingSpecial(){
+function NothingSpecial() {
     console.log("\nDon't mind me!");
 }
 
@@ -39,3 +39,33 @@ var a = new NothingSpecial();
 a;
 
 // b;`
+
+// inheritorio links
+function Foo(name) {
+    this.name = name;
+}
+
+Foo.prototype.myName = function () {
+    return this.name;
+}
+
+function Bar(name, label) {
+    Foo.call(this, name);
+    this.label = label;
+}
+
+// here, we make a new `Bar.prototype`
+// linked to `Foo.prototype`
+Bar.prototype = Object.create(Foo.prototype);
+
+// Beware! Now `Bar.prototype.constructor` is gone,
+// and might need to be manually "fixed" if you're
+// in the habit of relying on such properties!
+Bar.prototype.myLabel = function () {
+    return this.label;
+};
+
+var a = new Bar("a", "obj a");
+
+console.log(a.myName()); // "a"
+console.log(a.myLabel()); // "obj a"
