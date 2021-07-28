1af1a1ff85a1542feb69dafb7055df3a15219515
Serge-N <nserge@gmail.com>
2021-07-28 08:13:53 +0200

    OLOO

--- ydkjs/TO-BD-intro.js
+++ ydkjs/TO-BD-intro.js
@@ -1,1 +1,16 @@
 //use  OLOO (objects linked to other objects) instead of classes in js
+
+var Foo = {};
+
+var a1 = Object.create( Foo );
+
+console.log(a1);
+
+// check if chrome v8 uses .constructor.name to track objects
+
+Object.defineProperty(Foo,"constructor",{
+    enumerable: false,
+    value: function Gotcha(){}
+});
+
+console.log(a1);
