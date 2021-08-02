a5d88c9641f3f50804ed544e61fb4c4ce07969e7
Serge-N <nserge@gmail.com>
2021-08-02 12:33:39 +0200

    Types in Javascript.

---
+++ ydkjs/TandG-Types.js
@@ -0,0 +1,22 @@
+// types in Js
+
+// null - watch out as it is falsy at times.
+
+// undefined
+
+// boolean
+
+// number
+
+// string
+
+// object
+
+// symbol
+
+// arrays and functions are treated as objects.
+
+console.log(typeof typeof 42);
+
+// undefined is variable is one that has been declared in the accessible scope, but at the moment has no other value in it
+// undeclared variable is one that has not been formally declared in the accessible scope.
---
+++ ydkjs/TandG-values.js
@@ -0,0 +1,8 @@
+// this includes arrays, strings and numbers
+
+// arrays
+// you don't need to presize your arrays.
+
+// avoid empty slots
+
+//
