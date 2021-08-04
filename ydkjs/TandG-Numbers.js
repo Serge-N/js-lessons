dd0905f37fa301d1835a9cec8397462006eb3d87
Serge-N <nserge05@gmail.com>
2021-08-04 19:09:16 +0200

    NaN

--- ydkjs/TandG-Numbers.js
+++ ydkjs/TandG-Numbers.js
@@ -29,7 +29,7 @@ console.log(n);
 // be careful with fractional decimal values - use epsilon when comparing
 
 function numbersCloseEnoughToEqual(n1, n2) {
-    return Math.abs(n1 - n2) < Number.EPSILON;
+  return Math.abs(n1 - n2) < Number.EPSILON;
 }
 
 var m = 0.1 + 0.2;
@@ -40,3 +40,22 @@ console.log("Close enough: " + numbersCloseEnoughToEqual(m, o));
 console.log("Close enough: " + numbersCloseEnoughToEqual(.0000001, .0000002));
 
 // use big number utility when dealing with big number utiliy
+
+// special values
+
+// null is an empty value
+// undefined is a missing value
+
+// not a Number NaN -i.e an invalid number.
+// NaN means = I tried to perform a mathematic operation but failed, so here’s the failed number result instead
+
+var a = 2 / "foo";
+
+console.log("Is the result a number?" + a === NaN);
+
+
+//NaN != NaN always. - Use Number.isNaN() - to check for a valid number.
+// Dividing by 0 in Js results into infinity | IEE 754 floating-point
+
+var k = 0/-3; // -0
+var z = 0*-3 // -0 These result into negative zero.
