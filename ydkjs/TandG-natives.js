beceb8c99362c0ee4a2e424a6c8f4a924e53afde
Serge-N <nserge05@gmail.com>
2021-08-08 12:04:05 +0200

    errors and date.

--- ydkjs/TandG-natives.js
+++ ydkjs/TandG-natives.js
@@ -13,3 +13,16 @@ console.log( Object.prototype.toString.call( a ) );
 // primitive types are automatically boxed
 
 // avoid use new to e.g new string, leave the optisamastion to the js engine
+var date = new Date();
+
+console.log( date.toDateString() );
+
+function createError(x){
+    if(!x){
+        throw new Error("x wasn't provided");
+    } else{
+        console.log(`x is ${x}`)
+    }
+}
+
+createError( 3 );
