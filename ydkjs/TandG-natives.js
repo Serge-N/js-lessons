b0743e47cc66087c6869e5265ff468b8f95326e1
Serge-N <nserge05@gmail.com>
2021-08-10 22:51:57 +0200

    summary of types

--- ydkjs/TandG-natives.js
+++ ydkjs/TandG-natives.js
@@ -2,11 +2,11 @@
 
 var a = new String("abc");
 
-console.log( typeof a );
+console.log(typeof a);
 
-console.log ( a instanceof String );
+console.log(a instanceof String);
 
-console.log( Object.prototype.toString.call( a ) );
+console.log(Object.prototype.toString.call(a));
 
 // constructors create a primitive object.
 
@@ -15,14 +15,38 @@ console.log( Object.prototype.toString.call( a ) );
 // avoid use new to e.g new string, leave the optisamastion to the js engine
 var date = new Date();
 
-console.log( date.toDateString() );
+console.log(date.toDateString());
 
-function createError(x){
-    if(!x){
-        throw new Error("x wasn't provided");
-    } else{
-        console.log(`x is ${x}`)
-    }
+function createError(x) {
+  if (!x) {
+    throw new Error("x wasn't provided");
+  } else {
+    console.log(`x is ${x}`)
+  }
 }
 
-createError( 3 );
+createError(3);
+
+// symbols
+// designed basically for ES6 constructors
+// symbols are not objects they are simply scalar primitives
+
+
+// native prototypes
+
+// e.g string, this allows one to get methods from String.prototype
+var a = "auto boxed string";
+
+console.log(a.indexOf('x'));
+
+console.log(a.charAt(5));
+
+// all these methods create a new value without modifying the present one.
+
+//Common Protype types are:
+
+// Arrays
+// strings
+// Numbers
+// Functions
+// RegExp
