c42937afa32202290fea0e1c301f58dcab9584b1
Serge-N <nserge05@gmail.com>
2021-08-12 16:27:45 +0200

    explict coersion

--- ydkjs/TandG-Coercion.js
+++ ydkjs/TandG-Coercion.js
@@ -7,3 +7,19 @@ var a = 42;
 var b = a + ""; // implict
 
 var c = String(a); // explict
+
+// common corsion methods are toString, toNumber, toBoolean, ToPrimitive
+
+var d = [1, 2, 3];
+
+console.log(d.toString());
+
+// Json.stringify - does not work on functions, symbols, 
+
+var e = {
+    b: 42,
+    c: "42",
+    d: [1, 2, 3]
+};
+
+console.log( JSON.stringify(e) );
