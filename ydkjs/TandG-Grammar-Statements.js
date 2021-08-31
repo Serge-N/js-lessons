311395e61fe1f68abc1f909d3ca746674bb201ec
Serge-N <nserge05@gmail.com>
2021-08-31 21:56:46 +0200

    statement effects

--- ydkjs/TandG-Grammar-Statements.js
+++ ydkjs/TandG-Grammar-Statements.js
@@ -8,6 +8,19 @@ console.log(`The value ${b}`);
 // capturing  completion values
 var a, b;
 
-a = eval ("if (true) { b = 4 + 38;}");
+a = eval("if (true) { b = 4 + 38;}");
 
 console.log("The value " + a);
+
+function Vowels(str) {
+  var matches;
+  const regex = /[aeious]/g;
+
+  if (str && (matches = str.match(regex))){
+      return matches;
+    }
+}
+
+console.log( Vowels( "Hello World!") );
+
+// avoid labeled statements
