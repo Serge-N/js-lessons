b29b31c3d8a492990b659b63f9b5f31fcdfef994
Serge-N <nserge05@gmail.com>
2021-09-09 20:50:13 +0200

    async intro..

--- ydkjs/AsynC-Intro.js
+++ ydkjs/AsynC-Intro.js
@@ -11,4 +11,10 @@ var answer = now();
 
 setTimeout(later, 1000);
 
-// Event Loops
+// Event Loops 
+// Think of it terms of FIFO
+//  Js is a single threaded language that takes advantage of its host environment.
+
+// concurrency
+
+// a single thread loop is an example of concurrency
