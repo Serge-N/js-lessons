8396888a51885fe33c148749b14018cb21ca8502
Serge-N <nserge05@gmail.com>
2021-09-12 12:58:03 +0200

    callbacks intro.

---
+++ ydkjs/async-callbacks.js
@@ -0,0 +1,30 @@
+// callbacks can be nested
+// using third party control can be termed as IoC
+
+//unsafe
+function addNumbers(x, y) {
+  return x + y;
+}
+
+function addNumbersBetter(x, y) {
+  if (typeof x != "number" || typeof y != "number") {
+    throw new Error("Bad parameters");
+  }
+  return x + y;
+}
+
+function addNumbersBest(x, y) {
+  x = Number(x);
+  y = Number(y);
+
+  return x + y;
+}
+
+console.log(addNumbers(4, 5));
+console.log(addNumbers(2, "5"));
+console.log(addNumbersBest(2, 5));
+console.log(addNumbersBest(2, "5"));
+console.log(addNumbersBetter(2, 5));
+//console.log(addNumbersBetter(2, "5"));
+
+// trust but verify
