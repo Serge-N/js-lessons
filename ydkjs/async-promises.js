84b9483ceff0f94b5c30c8bc6d5f8f700a727fe8
Serge-N <nserge05@gmail.com>
2021-09-14 23:35:40 +0200

    base promise

--- ydkjs/async-promises.js
+++ ydkjs/async-promises.js
@@ -1,1 +1,7 @@
 // in api's sometimes a promise is never returned...
+
+// a promise has a fullfillment handler
+// it alos has a rejection handler
+// once a promise is resolved it stays that way foever. - an immutable vale
+// a promise promotes separtion of concerns in that ithe called function is not aware of whats going on.
+// the return of a promise that has a new is called the "revealing constructor"
