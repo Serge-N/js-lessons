b39b56d777c1e4988b9d4fc55b67ee9da07a901d
Serge-N <nserge05@gmail.com>
2021-09-24 11:07:03 +0200

    conclusion book async

--- ydkjs/async-promise-example-1.js
+++ ydkjs/async-promise-example-1.js
@@ -8,3 +8,40 @@
 // The natural way to get the most out of Promises and generators is to yield
 //  a Promise, and wire that Promise to control the generator's iterator.
 // ES 7 supports asnc and await
+// write optimal code senting request in parallel if possible.
+// make both requests "in parallel," and
+// wait until both promises resolve
+
+// hide promise based code for clean approach. e.g
+
+// normal function 
+function bar(url1, url2) {
+    return Promise.all([
+        request(url1),
+        request(url2)
+    ]);
+}
+
+function* foo() {
+    var results = yield bar(
+        "http://some.url.1",
+        "http://some.url.2"
+    );
+
+    var r1 = results[0];
+    var r1 = results[1];
+
+    var r3 = yield request (
+        "http://some.url.3/?v=" + r1 + "," + r2
+    );
+
+    console.log(r3);
+}
+
+// utility i.e a generator with an iterator
+run (foo);
+
+// a generator can be used to call another genertaor
+
+// Conclusion skip to last book.  
+// Come back and continue here only condition that deeper practical understanding is attentained. - water too deep. 
