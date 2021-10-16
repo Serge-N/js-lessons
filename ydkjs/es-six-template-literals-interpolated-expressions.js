79864ff5577a2dc4000163079fd5359389904b8f
Serge-N <nserge05@gmail.com>
2021-10-16 21:00:45 +0000

    interplation strings in js

---
+++ ydkjs/es-six-template-literals-interpolated-expressions.js
@@ -0,0 +1,15 @@
+// these are simply resusble piecies of text
+const userName = "Timothy";
+
+var greeting = `Hello ${userName}`;
+
+console.log(greeting);
+console.log(typeof greeting);
+
+// they are multiline.
+
+var text =`Now is the time for all good men
+to come to the aid of their
+country!`
+
+console.log(text);
